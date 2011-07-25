1d2d08ff9b91185d0f1777ac858b28e58e2150f9
joe <joe@5b5bb49e-a627-11e0-b536-939dc24ca361>
2011-07-25 12:30:54 +0000

    git-svn-id: http://svn.joeshort.net/modules@8 5b5bb49e-a627-11e0-b536-939dc24ca361

--- slides/templatetags/slides_tags.py
+++ slides/templatetags/slides_tags.py
@@ -1,12 +1,15 @@
 from django import template
 from slides.models import Slide
+import datetime
 
 register = template.Library()
 
 @register.inclusion_tag('slides/includes/slideshow.html')
 def slideshow():
+
     """Render a crossfading slideshow"""
 
-    c = {}
+    today = datetime.date.today()
+    slides = Slide.objects.filter(show_from__gte=today)     #TODO: Add show_until if not blank
 
-    return c
+    return { 'slides' : slides }
