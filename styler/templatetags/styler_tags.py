2dc52e0eead0a5bca4b290a0fb32bd3bbb624391
joe <joe@5b5bb49e-a627-11e0-b536-939dc24ca361>
2011-10-20 14:30:37 +0000

    cross browser round corners and overlinks
    git-svn-id: http://svn.joeshort.net/modules@16 5b5bb49e-a627-11e0-b536-939dc24ca361

--- styler/templatetags/styler_tags.py
+++ styler/templatetags/styler_tags.py
@@ -14,31 +14,41 @@ def make_rounded_top(cr) :
     return """-webkit-border-top-left-radius:%spx;
 -webkit-border-top-right-radius:%spx;
 -moz-border-radius-topleft:%spx;
--moz-border-radius-topright:%spx;""" % (cr,cr,cr,cr)
+-moz-border-radius-topright:%spx;
+border-top-left-radius:%spx;
+border-top-right-radius:%spx;""" % (cr,cr,cr,cr,cr,cr)
 
 @register.simple_tag
 def make_rounded_left(cr) :
     return """-webkit-border-top-left-radius:%spx;
 -webkit-border-bottom-left-radius:%spx;
 -moz-border-radius-topleft:%spx;
--moz-border-radius-bottomleft:%spx;""" % (cr,cr,cr,cr)
+-moz-border-radius-bottomleft:%spx;
+border-top-left-radius:%spx;
+border-bottom-left-radius:%spx;""" % (cr,cr,cr,cr,cr,cr)
 
 @register.simple_tag
 def make_rounded_right(cr) :
     return """-webkit-border-top-right-radius:%spx;
 -webkit-border-bottom-right-radius:%spx;
 -moz-border-radius-topright:%spx;
--moz-border-radius-bottomright:%spx;""" % (cr,cr,cr,cr)
+-moz-border-radius-bottomright:%spx;
+border-top-right-radius:%spx;
+border-bottom-right-radius:%spx;""" % (cr,cr,cr,cr,cr,cr)
 
 @register.simple_tag
 def make_rounded_bottom(cr) :
     return """-webkit-border-bottom-left-radius:%spx;
 -webkit-border-bottom-right-radius:%spx;
 -moz-border-radius-bottomleft:%spx;
--moz-border-radius-bottomright:%spx;""" % (cr,cr,cr,cr)
+-moz-border-radius-bottomright:%spx;
+border-bottom-left-radius:%spx;
+border-bottom-right-radius:%spx;""" % (cr,cr,cr,cr,cr,cr)
 
 @register.simple_tag
 def make_rounded(cr) :
-    return '-webkit-border-radius: %spx;\n-moz-border-radius: %spx;\n' % (cr,cr)
+    return """-webkit-border-radius: %spx;
+-moz-border-radius: %spx;
+border-radius:%spx;""" % (cr,cr,cr)
 
 
