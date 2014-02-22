0b54c1f6fc490888c830faac2b16844e94688110
joe <joe@5b5bb49e-a627-11e0-b536-939dc24ca361>
2014-02-22 23:47:47 +0000

    removed quicklinks
    git-svn-id: http://svn.joeshort.net/modules@22 5b5bb49e-a627-11e0-b536-939dc24ca361

--- quicklinks/admin.py
+++
@@ -1,8 +0,0 @@
-from quicklinks.models import *
-from django.contrib import admin
-
-class QuickLinkAdmin(admin.ModelAdmin):
-    list_display = ['__unicode__', 'order', 'url']
-
-
-admin.site.register(QuickLink, QuickLinkAdmin)
